11c0d68edc3c75541cc22fe7e64dc980db0f1fc1
ThaisCampanac <thais.campanac@gmail.com>
2021-08-10 20:50:07 -0500

    feat: Add mobile styling for faq.tsx
    The faq.tsx mobile page is now styled for mobile viewing through configurations
    of globals.css.

--- components/MobileLinks.tsx
+++ components/MobileLinks.tsx
@@ -4,16 +4,28 @@ export default function MobileLinks() {
   return (
     <div className="SidebarLinks" id="SidebarLinks">
       <Link href="/schedule">
-        <a className="link">Schedule</a>
+        <div className="link">
+          <span className="scheduledot"></span>
+          <a>Schedule</a>
+        </div>
       </Link>
       <Link href="/speakers">
-        <a className="link">Speakers</a>
+        <div className="link">
+          <span className="speakerdot"></span>
+          <a>Speakers</a>
+        </div>
       </Link>
       <Link href="/sponsors">
-        <a className="link">Sponsors</a>
+        <div className="link">
+          <span className="sponsordot"></span>
+          <a>Sponsors</a>
+        </div>
       </Link>
       <Link href="/faq">
-        <a className="link">FAQ</a>
+        <div className="link">
+          <span className="faqdot"></span>
+          <a>FAQ</a>
+        </div>
       </Link>
       <button className="SigninButton">Sign-in</button>
     </div>
