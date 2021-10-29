675d711dc8ea296b20304c957b17ef7ea0cd49a2
Abdullah Hasani <abdullah50053@gmail.com>
2021-10-29 12:14:47 -0500

    feat(sponsors): Add sponsor images
    re #12

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
+const withImages = require('next-images');
 
-module.exports = withPWA({
-  reactStrictMode: true,
-  images: {
-    domains: ['lh3.googleusercontent.com'],
+module.exports = withPWA(
+  {
+    reactStrictMode: true,
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
+    pwa: {
+      dest: 'public',
+      runtimeCaching,
+      disable: !process.env.ENABLE_PWA && process.env.NODE_ENV === 'development',
+    },
   },
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    disable: !process.env.ENABLE_PWA && process.env.NODE_ENV === 'development',
-  },
-});
+  (module.exports = withImages({
+    fileExtensions: ['jpg', 'jpeg', 'png', 'gif'],
+    webpack(config, options) {
+      return config;
+    },
+  })),
+);
