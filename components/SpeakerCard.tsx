9dd14970bd175867bcd28c713e01a163fcc5fd45
Abdullah Hasani <abdullah50053@gmail.com>
2021-11-08 08:47:28 -0600

    chore(speaker): Add speaker card and info
    TODO: make card mobile friendly

---
+++ components/SpeakerCard.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Image from 'next/image';
+
+/**
+ * Speaker Cards Component
+ *
+ * Cards for Speaker section
+ */
+// ${props.path}
+function SpeakerCard(props) {
+  return (
+    <>
+      <div id="SpeakerCards" className="flex flex-wrap justify-center">
+        <div className="h-[22rem] w-[60rem] mx-5 my-3 flex border-pink border-2 rounded-lg">
+          <div className="h-[20rem] w-[160rem] flex flex-center justify-center p-4 ">
+            <Image
+              className="rounded-lg border-blue border-2"
+              src={`/../pages/speakers.tsx/..${props.path}`}
+              alt="Speaker Picture"
+              width={500}
+              height={500}
+            />
+          </div>
+          <div className="h-1/3 p-8">
+            <h1 className="text-3xl">{props.name}</h1>
+            <h2 className="text-xl">{props.company}</h2>
+            <p className="text-sm">{props.description}</p>
+            <h3 className="text-sm">{props.website}</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SpeakerCard;
--- pages/speakers.tsx
+++ pages/speakers.tsx
@@ -1,12 +1,26 @@
-import MobileHeader from '../components/MobileHeader';
-import MobileLinks from '../components/MobileLinks';
+import SpeakerCard from '../components/SpeakerCard';
 
 export default function speakers() {
   return (
     <div className="speakersPage">
-      <div className="speakersMainPage">
+      <div className="speakersMainPage flex flex-col items-center">
         <h4 className="speakersTitle">Speakers</h4>
-        <h5>Come back later for some good news!</h5>
+        <div id="SpeakerCards" className="flex flex-col gap-y-4 w-full">
+          {/* path is relative path from speakers.tsx file(this file) to assets*/}
+          {/* general path -  /../public/assets/speakerImages/{speaker file}*/}
+          <SpeakerCard
+            path="/../public/assets/speaker.jpg"
+            name="Paul Bailo"
+            company="OrangeShine"
+            description="Paul Bailo is widely recognized by industry thought leaders for his C-level executive skills in Digital Transformation, Marketing and Business Operations. 
+            He has earned deep career success in multiple fields, along with an outstanding industry reputation and important executive roles as Digital Innovator, Strategic Thinker, 
+            Change Agent, Customer Champion, Industry Author and Creative Entrepreneur. He has built and led top performing teams, start-up entities and world-class organizations. 
+            His background in digital transformation, digital communications, emerging payments, operational excellence, data-driven decision-making and ongoing product innovation is unparalleled. 
+            His blue-chip background encompasses Google, Apple, ATT, Bank of America, 
+            Goldman Sachs, MasterCard, American Express, GE, Citibank, Federal Reserve Bank of New York, NYC Transit Authority and US Department of Transportation."
+            website="linkedin.com/in/paulbailo"
+          />
+        </div>
       </div>
     </div>
   );
