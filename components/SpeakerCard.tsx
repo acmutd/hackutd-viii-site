072fcf74962eac8d5db4fc5b1d1d0e2d606882d7
nam-t24 <namtruong831@gmail.com>
2021-11-08 14:48:36 -0600

    feat: Add full page speaker cards
    Added larger speaker cards for large screens from chore/speaker branch. Large
    landscape cards will be displayed for large screens while small portrait cards
    will be displayed for screens smaller than medium.

--- components/SpeakerCard.tsx
+++ components/SpeakerCard.tsx
@@ -6,11 +6,29 @@ import Image from 'next/image';
  *
  * Cards for Speaker section
  */
-// ${props.path}
 function SpeakerCard(props) {
   return (
     <>
-      <div id="SpeakerCards" className="flex flex-wrap justify-center">
+      <div id="SpeakerCards" className="hidden md:flex flex-wrap justify-center w-full">
+        <div className="h-[22rem] w-full mx-5 my-3 flex border-pink border-2 rounded-lg">
+          <div className="h-[20rem] w-[160rem] flex flex-center justify-center p-4 ">
+            <Image
+              className="rounded-lg border-blue border-2"
+              src={`/../pages/speakers.tsx/..${props.path}`}
+              alt="Speaker Picture"
+              width={500}
+              height={500}
+            />
+          </div>
+          <div className="h-1/3 p-8">
+            <h1 className="text-3xl">{props.name}</h1>
+            <h2 className="text-xl">{props.company}</h2>
+            <p className="text-sm">{props.description}</p>
+            <h3 className="text-sm">{props.website}</h3>
+          </div>
+        </div>
+      </div>
+      <div id="SpeakerCards" className=" md:hidden flex flex-wrap justify-center">
         <div className="h-[26rem] w-[18rem] mx-5 my-3 flex flex-col border-pink border-2 rounded-lg">
           <div className="h-2/5 flex justify-center items-center">
             <Image
