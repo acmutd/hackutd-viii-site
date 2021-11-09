b641749134b3ad0462defc9102253051506d3ca1
Abdullah Hasani <abdullah50053@gmail.com>
2021-11-09 13:21:40 -0600

    chore(speaker): Add second speaker

--- pages/speakers.tsx
+++ pages/speakers.tsx
@@ -9,7 +9,7 @@ export default function speakers() {
           {/* path is relative path from speakers.tsx file(this file) to assets*/}
           {/* general path -  /../public/assets/speakerImages/{speaker file}*/}
           <SpeakerCard
-            path="/../public/assets/speaker.jpg"
+            path="/assets/speakerImages/speaker.jpg"
             name="Paul Bailo"
             company="OrangeShine"
             description="Paul Bailo is widely recognized by industry thought leaders for his C-level executive skills in Digital Transformation, Marketing and Business Operations. 
@@ -19,6 +19,24 @@ export default function speakers() {
             His blue-chip background encompasses Google, Apple, ATT, Bank of America, 
             Goldman Sachs, MasterCard, American Express, GE, Citibank, Federal Reserve Bank of New York, NYC Transit Authority and US Department of Transportation."
             website="linkedin.com/in/paulbailo"
+          />
+        </div>
+        <div id="SpeakerCards" className="flex flex-wrap justify-center md:w-5/6 w-screen">
+          {/* path is relative path from speakers.tsx file(this file) to assets*/}
+          {/* general path -  /../public/assets/speakerImages/{speaker file}*/}
+          <SpeakerCard
+            path="/assets/speakerImages/speaker2.png"
+            name="Suresh Thumma"
+            company="Goldman Sachs"
+            description="Suresh Thumma is a Managing Director and Tech Fellow at Goldman Sachs. 
+            He manages software engineering for cloud enablement, which enables business application deployments on public cloud.
+             Before rejoining Goldman Sachs, Suresh was co-founder and chief executive officer of StackLynx, 
+             an online platform for deploying and managing micro services on public cloud. Prior to that, 
+             he was a vice president and global head of Cloud Platforms and Platform Automation at Visa. 
+             Earlier in his career, Suresh held a variety of roles, including director of Software Engineering 
+             for the Investment Research Division at Merrill Lynch and development manager at IBM. 
+             He first joined Goldman Sachs in 2008."
+            website="https://www.linkedin.com/in/suresh-thumma-9a67086/"
           />
         </div>
       </div>
