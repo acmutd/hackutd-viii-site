1b0f978a7a92c907d4ecf89f71eab9bdb6c6b895
Abdullah Hasani <abdullah50053@gmail.com>
2021-11-09 13:30:43 -0600

    fix(speakers): Fix speaker mobile view

--- components/SpeakerCard.tsx
+++ components/SpeakerCard.tsx
@@ -6,13 +6,13 @@ import Image from 'next/image';
  *
  * Cards for Speaker section
  */
-// ${props.path}
 function SpeakerCard(props) {
+  const result = props.website.replace(/(^\w+:|^)\/\//, '');
   return (
     <>
-      <div id="SpeakerCards" className="flex flex-wrap justify-center">
-        <div className="h-[22rem] w-[60rem] mx-5 my-3 flex border-pink border-2 rounded-lg">
-          <div className="h-[20rem] w-[160rem] flex flex-center justify-center p-4 ">
+      <div id="SpeakerCards" className="hidden sm:flex flex-wrap justify-center w-full">
+        <div className="min-h-[22rem] w-full mx-5 my-3 flex border-pink border-2 rounded-lg">
+          <div className=" min-w-[18rem] flex flex-col items-center p-4 ">
             <Image
               className="rounded-lg border-blue border-2"
               src={props.path}
@@ -20,12 +20,38 @@ function SpeakerCard(props) {
               width={500}
               height={500}
             />
+            <br></br>
+            <p className="text-2xl lg:hidden inline">{props.name}</p>
+            <p className="text-xl lg:hidden inline">{props.company}</p>
+            <a href={props.website} target="_blank" rel="noreferrer" className="text-sm">
+              {result}
+            </a>
           </div>
-          <div className="h-1/3 p-8">
-            <h1 className="text-3xl">{props.name}</h1>
-            <h2 className="text-xl">{props.company}</h2>
+          <div className="h-1/3 p-8 flex flex-col">
+            <h1 className="text-3xl hidden lg:inline">{props.name}</h1>
+            <h2 className="text-xl hidden lg:inline">{props.company}</h2>
             <p className="text-sm">{props.description}</p>
-            <h3 className="text-sm">{props.website}</h3>
+          </div>
+        </div>
+      </div>
+      <div id="SpeakerCards" className="sm:hidden flex flex-wrap justify-center">
+        <div className="min-h-[26rem] min-w-[18rem] mx-5 my-3 flex flex-col border-pink border-2 rounded-lg">
+          <div className="h-2/5 flex justify-center items-center pt-2">
+            <Image
+              className="rounded-xl"
+              src={props.path}
+              alt="Speaker Picture"
+              width={150}
+              height={150}
+            />
+          </div>
+          <div className="p-5">
+            <h1>{props.name}</h1>
+            <h2>{props.company}</h2>
+            <p className="text-xs">{props.description}</p>
+            <a href={props.website} target="_blank" rel="noreferrer" className="text-sm">
+              {result}
+            </a>
           </div>
         </div>
       </div>
