93a677a6d9d351270fe6c990185fcfa4bca06c67
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-11-11 21:18:38 -0600

    fix: Speakers styling

--- pages/speakers.tsx
+++ pages/speakers.tsx
@@ -3,7 +3,7 @@ import SpeakerCard from '../components/SpeakerCard';
 export default function speakers() {
   return (
     <div className="speakersPage">
-      <div className="speakersMainPage flex flex-col items-center">
+      <div className="speakersMainPage flex flex-col items-center max-w-6xl mx-auto">
         <h4 className="speakersTitle">Speakers</h4>
         <div id="SpeakerCards" className="flex flex-wrap justify-center md:w-5/6 w-screen">
           {/* general path -  /assets/speakerImages/{speaker file}*/}
@@ -18,12 +18,13 @@ export default function speakers() {
             His background in digital transformation, digital communications, emerging payments, operational excellence, data-driven decision-making and ongoing product innovation is unparalleled. 
             His blue-chip background encompasses Google, Apple, ATT, Bank of America, 
             Goldman Sachs, MasterCard, American Express, GE, Citibank, Federal Reserve Bank of New York, NYC Transit Authority and US Department of Transportation."
-            website="linkedin.com/in/paulbailo"
+            website="https://www.linkedin.com/in/paulbailo"
           />
           <SpeakerCard
             path="/assets/speakerImages/SureshThumma-GS.png"
             name="Suresh Thumma"
-            company="Managing Director - Goldman Sachs"
+            company="Goldman Sachs"
+            role="Managing Director"
             description="Suresh Thumma is a Managing Director and Tech Fellow at Goldman Sachs. 
             He manages software engineering for cloud enablement, which enables business application deployments on public cloud.
              Before rejoining Goldman Sachs, Suresh was co-founder and chief executive officer of StackLynx, 
