21114378144ddc2520ac5d8fd0b39c583632d4ec
jariel97 <jariel.gojar@gmail.com>
2021-11-11 18:37:37 -0600

    fix: Location display in event pop up

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -273,7 +273,7 @@ const resources = [
     fieldName: 'location',
     title: 'Location',
     instances: [
-      { id: 'Room 1', text: 'Room 1', color: indigo },
+      { id: 'ECSW', text: 'ECSW ', color: indigo },
       { id: 'Room 2', text: 'Room 2', color: blue },
       { id: 'Room 3', text: 'Room 3', color: teal },
     ],
